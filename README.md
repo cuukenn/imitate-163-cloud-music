06f2aa19843201b6a61381e40adbd7a8614ef56c
cgg <891842749@qq.com>
2018-06-16 22:03:47 +0800

    添加界面框架 修改readme描述

--- README.md
+++ README.md
@@ -8,8 +8,12 @@
 -路由控制：vue-router
 -字体图标来源：阿里矢量图标库
 -目标：实现网易云大体界面和相关功能
--数据来源：考虑使用vue axios编写爬虫获取
-
+-数据来源：感谢github大神们：https://github.com/Binaryify/NeteaseCloudMusicApi
+-用法
+下载数据接口服务：https://github.com/Binaryify/NeteaseCloudMusicApi
+下载该项目
+解压NeteaseCloudMusicApi后命令行进入到eteaseCloudMusicApi  输入node app.js
+解压该项目包后命令行进入到对应目录  输入npm/cnpm install ，完成后输入npm/cnpm run dev启动项目
 #### 进度示例
 ![Image text](https://gitee.com/cgggitee/h5_vue_wangyiyun/raw/master/img-folder/1.png)
 ![Image text](https://gitee.com/cgggitee/h5_vue_wangyiyun/raw/master/img-folder/2.png)
--- src/page/menuAside/myMessage.vue
+++ src/page/menuAside/myMessage.vue
@@ -1,25 +1,31 @@
 <template>
   <div>
     <x-header>
-      <i></i>
     </x-header>
+    <tab bar-position="top">
+      <tab-item selected @on-item-click="">私信</tab-item>
+      <tab-item @on-item-click="">评论</tab-item>
+      <tab-item @on-item-click="">@我</tab-item>
+      <tab-item @on-item-click="">通知</tab-item>
+    </tab>
   </div>
 </template>
 <style lang="less">
 
 </style>
 <script>
-  import {XHeader,Tab,TabItem,Group,Cell} from 'vux';
+  import {XHeader, Tab, TabItem, Group, Cell} from 'vux';
+
   export default {
-    name:'myMessage',
-    components:{
+    name: 'myMessage',
+    components: {
       XHeader,
-      Tab,TabItem,
-      Group,Cell
+      Tab, TabItem,
+      Group, Cell
     },
-    props:{
-      title:{
-        type:String
+    props: {
+      title: {
+        type: String
       }
     }
   }
